f9be38ec08a58351a16d57d82272de710d71c2b1
ChienLinSu <dan199537@gmail.com>
2026-04-04 23:40:14 +0800

    fix: force light mode regardless of OS dark mode setting

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
+#include <QStyleHints>
 
 #include "LVGLSimulator.h"
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
+  a.styleHints()->setColorScheme(Qt::ColorScheme::Light);
 
   MainWindow w;
   LVGLKeyPressEventFilter filter(w.simulator(), &a);
