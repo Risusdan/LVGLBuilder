7bfcc3120a380c42310c2fe13ba504d3608a156e
ChienLinSu <dan199537@gmail.com>
2026-04-16 22:53:16 +0800

    test: add failing LEFT/RIGHT tests for tabview remove (C1, C2)

--- tests/tst_tabview_remove/tst_tabview_remove.cpp
+++ tests/tst_tabview_remove/tst_tabview_remove.cpp
@@ -322,6 +322,63 @@ class TestTabviewRemove : public QObject {
     QCOMPARE(tvObj->childs().at(0)->index(), 0);
     QCOMPARE(tvObj->childs().at(1)->index(), 1);
   }
+
+  // =================================================================
+  // Round 5: LEFT/RIGHT button position tests (C1, C2 bug coverage)
+  // =================================================================
+
+  void removeMiddleTabLeft() {
+    lv_obj_t *screen = lv_scr_act();
+    lv_obj_t *tv = lv_tabview_create(screen, nullptr);
+    lv_tabview_set_btns_pos(tv, LV_TABVIEW_BTNS_POS_LEFT);
+    lv_tabview_add_tab(tv, "A");
+    lv_tabview_add_tab(tv, "B");
+    lv_tabview_add_tab(tv, "C");
+
+    lv_tabview_remove_tab(tv, 1);
+
+    QCOMPARE(lv_tabview_get_tab_count(tv), static_cast<uint16_t>(2));
+    lv_tabview_ext_t *ext = reinterpret_cast<lv_tabview_ext_t *>(
+        lv_obj_get_ext_attr(tv));
+    QCOMPARE(QString(ext->tab_name_ptr[0]), QString("A"));
+    QCOMPARE(QString(ext->tab_name_ptr[2]), QString("C"));
+
+    lv_obj_del(tv);
+  }
+
+  void removeFirstTabRight() {
+    lv_obj_t *screen = lv_scr_act();
+    lv_obj_t *tv = lv_tabview_create(screen, nullptr);
+    lv_tabview_set_btns_pos(tv, LV_TABVIEW_BTNS_POS_RIGHT);
+    lv_tabview_add_tab(tv, "A");
+    lv_tabview_add_tab(tv, "B");
+    lv_tabview_add_tab(tv, "C");
+
+    lv_tabview_remove_tab(tv, 0);
+
+    QCOMPARE(lv_tabview_get_tab_count(tv), static_cast<uint16_t>(2));
+    lv_tabview_ext_t *ext = reinterpret_cast<lv_tabview_ext_t *>(
+        lv_obj_get_ext_attr(tv));
+    QCOMPARE(QString(ext->tab_name_ptr[0]), QString("B"));
+    QCOMPARE(QString(ext->tab_name_ptr[2]), QString("C"));
+
+    lv_obj_del(tv);
+  }
+
+  void propertyGetLeft() {
+    lv_obj_t *screen = lv_scr_act();
+    const LVGLWidget *tvWidget = lvgl.widget("lv_tabview");
+    LVGLObject *tvObj = new LVGLObject(tvWidget, "test_tv_left", screen);
+    lvgl.addObject(tvObj);
+
+    lv_tabview_set_btns_pos(tvObj->obj(), LV_TABVIEW_BTNS_POS_LEFT);
+
+    LVGLProperty *tabsProp = tvWidget->property("Tabs");
+    tabsProp->setValue(tvObj, QVariant(QVariantList{{"A"}, {"B"}, {"C"}}));
+
+    QVariant v = tabsProp->value(tvObj);
+    QCOMPARE(v.toString(), QString("A, B, C"));
+  }
 };
 
 QTEST_MAIN(TestTabviewRemove)
